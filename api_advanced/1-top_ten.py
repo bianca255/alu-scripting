ec7a4a68661f74332f258eac8b3777073f778653
bianca255 <b.haguma@alustudent.com>
2025-02-24 01:48:07 -0800

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,13 +1,22 @@
-#!/usr/bin/python3
-"""
-1-main
-"""
-import sys
+import requests
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+def top_ten(subreddit):
+    """Print the top 10 hot posts from a subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'myapp'}
+    
+    # Send the GET request to Reddit API
+    response = requests.get(url, headers=headers)
+
+    # Check if the subreddit exists (valid subreddit)
+    if response.status_code == 200:
+        # Parse the response data
+        posts = response.json()['data']['children']
+        
+        # Print the titles of the top 10 hot posts
+        for post in posts:
+            print(post['data']['title'])
     else:
-        top_ten(sys.argv[1])
+        # Print None if the subreddit is invalid
+        print(None)
 
