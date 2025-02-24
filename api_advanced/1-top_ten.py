99842b519a7154514611fd80bdee85498fb5718b
bianca255 <b.haguma@alustudent.com>
2025-02-24 02:04:42 -0800

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,22 +4,13 @@ import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed in a subreddit."""
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    
-    # Send the GET request to Reddit API
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    # Output check: non-existent subreddit
     if response.status_code != 200:
         print(None)
         return
-    
-    # Output check: existing subreddit
     posts = response.json()['data']['children']
-    
-    # Print the titles of the top 10 hot posts
     for post in posts:
         print(post['data']['title'])
-
