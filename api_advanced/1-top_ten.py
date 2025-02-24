4c1258aacd219b1a40d6e4f2c7dc6d56ae6149ef
bianca255 <b.haguma@alustudent.com>
2025-02-24 02:48:18 -0800

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,8 +9,8 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
-        print("OK", end="")
+        print(None)
         return
-    posts = response.json()['data']['children']            
+    posts = response.json()['data']['children']
     for post in posts:
         print(post['data']['title'])
