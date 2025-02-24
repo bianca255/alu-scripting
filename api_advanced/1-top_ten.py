bf90176696390563028366b9def4aebe502f22b0
bianca255 <b.haguma@alustudent.com>
2025-02-24 01:53:02 -0800

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,34 +1,16 @@
 #!/usr/bin/python3
-"""
-This script queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
-
-Usage:
-Call the `top_ten` function with a subreddit name as a parameter.
-
-Example:
-top_ten('programming')
-"""
-
+""" 1-top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """Print the top 10 hot posts from a subreddit."""
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'myapp'}
-    
-    # Send the GET request to Reddit API
-    response = requests.get(url, headers=headers)
-
-    # Check if the subreddit exists (valid subreddit)
-    if response.status_code == 200:
-        # Parse the response data
-        posts = response.json()['data']['children']
-        
-        # Print the titles of the top 10 hot posts
-        for post in posts:
-            print(post['data']['title'])
-    else:
-        # Print None if the subreddit is invalid
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
-
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
